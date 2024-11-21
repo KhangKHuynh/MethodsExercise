be17fba7da06ecf7dd1a1a0214ece6452529c355
Khang Huynh <kkyhuynh@gmail.com>
2024-11-21 13:21:38 -0600

    did both c# story and Math operations assignment

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,43 @@
 {
     public class Program
     {
+        public static void MadLib()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string userColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string userAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string userBand = Console.ReadLine();
+            
+            Console.WriteLine($"Yesterday, {userName} went on a date and was wearing a {userColor} shirt with a {userAnimal} on it. On the way to the date he was listening to {userBand} to hype himself up)");
+        }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLib();
+            
         }
     }
 }
